1cbbfbd97bdcac255110a1d1996b385788c6e1d3
M-Muhaddis-Irshad <muhaddisirshad58@gmail.com>
2025-08-26 04:33:05 +0500

    Add validations on signup page

--- app.js
+++ app.js
@@ -4,7 +4,51 @@ const user_name = document.getElementById('user_name')
 const signup_mail = document.getElementById('user_signup_email')
 const signup_password = document.getElementById('user_signup_password')
 
+// console.log(document.getElementsByTagName('button')[0]);
+// console.log(document.getElementsByTagName('button')[1]);
+
+const email_regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
 function signup_btn() {
+
+    if (!user_name.value.trim() || !signup_mail.value.trim() || !signup_password.value.trim()) {
+        Swal.fire({
+            icon: "error",
+            title: "Error! All fields are required",
+            text: "Something went wrong!",
+        });
+        return;
+    }
+    else if (!email_regex.test(signup_mail.value)) {
+        Swal.fire({
+            icon: "error",
+            title: "Error! Enter valid email address",
+            text: "Something went wrong!",
+        });
+        // console.error("Enter valid email address");
+        return
+    }
+    else if (signup_mail.value === localStorage.getItem("User Email")) {
+        teleporter(params = "sgnup_to_lgn")
+        Swal.fire({
+            icon: "error",
+            title: "Please login \nThis email is already registered",
+            text: "Something went wrong!",
+        });
+        return
+    }
+    else {
+        // console.log("Corrct email");
+        localStorage.setItem("User Name", user_name.value)
+        localStorage.setItem("User Email", signup_mail.value)
+        localStorage.setItem("User Password", signup_password.value)
+
+        Swal.fire({
+            title: "Account successfully created",
+            icon: "success",
+            draggable: true
+        });
+    }
 
 }
 
@@ -13,8 +57,12 @@ function signup_btn() {
 
 // Login Btn Start's__________________________
 
-const login_mail = document.getElementById('user_login_email')
-const login_password = document.getElementById('user_login_password')
+// const login_mail = document.getElementById('user_login_email')
+// const login_password = document.getElementById('user_login_password')
+
+// function login_btn(params) {
+
+// }
 
 // Login Btn End's__________________________
 
