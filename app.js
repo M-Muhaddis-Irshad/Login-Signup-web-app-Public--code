30964d38ccb5a2473974d1d9aa6123650673f258
M-Muhaddis-Irshad <muhaddisirshad58@gmail.com>
2025-08-28 20:22:41 +0500

    Login/Sign up page completed
    Login & Registration/Signup page with all validation responsive UI for all screens that is equal & bigger than tablets ,I'll responsive it for mobiles to.

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const email_regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 
 function signup_btn() {
 
+
     if (!user_name.value.trim() || !signup_mail.value.trim() || !signup_password.value.trim()) {
         Swal.fire({
             icon: "error",
@@ -22,23 +23,34 @@ function signup_btn() {
     else if (!email_regex.test(signup_mail.value)) {
         Swal.fire({
             icon: "error",
-            title: "Error! Enter valid email address",
+            title: "Error! Invalid email address",
             // text: "Something went wrong!",
         });
         // console.error("Enter valid email address");
         return
     }
-    else if (signup_mail.value === localStorage.getItem("User Email")) {
-        teleporter(params = "sgnup_to_lgn")
+    else if (localStorage.getItem('User Name') !== null) {
         Swal.fire({
             icon: "error",
-            title: "This email is already registered",
-            text: "Please login",
+            title: `You're already registered with ${localStorage.getItem('User Email')}`,
+            html: `If you want to register again, first delete your previous account by clicking on this <span onclick="del_acnt()"><a href="#"> Delete Account </a></span> and then create a new one.`,
         });
-        return
+
+        empty_inputs()
+
+        return;
     }
+    // else if (signup_mail.value === localStorage.getItem("User Email")) {
+    //     teleporter(params = "sgnup_to_lgn")
+    //     Swal.fire({
+    //         icon: "error",
+    //         title: "This email is already registered",
+    //         text: "Please login",
+    //     });
+    //     return
+    // }
     else {
-        // console.log("Corrct email");
+
         localStorage.setItem("User Name", user_name.value)
         localStorage.setItem("User Email", signup_mail.value)
         localStorage.setItem("User Password", signup_password.value)
@@ -48,11 +60,29 @@ function signup_btn() {
             icon: "success",
             draggable: true
         });
+
+        teleporter(params = "sgnup_to_lgn")
+
     }
 
 }
 
 // Sign Up Btn End's__________________________
+
+
+// Account Delete_____________________________________________
+
+function del_acnt() {
+    localStorage.clear()
+    Swal.fire({
+        icon: "success",
+        title: "The account has been successfully deleted",
+        text: "You can now re-register",
+    });
+    return;
+}
+
+// Account Delete_____________________________________________
 
 
 // Login Btn Start's__________________________
@@ -62,24 +92,47 @@ const login_password = document.getElementById('user_login_password')
 
 function login_btn() {
 
-    if (!login_mail.value.trim() || !login_password.value.trim()) {
+    if (localStorage.getItem('User Name') === null) {
+        Swal.fire({
+            icon: "error",
+            title: "No existing account detected",
+            text: "Please register to continue"
+        });
+
+        teleporter(params = "lgn_to_sgnup")
+        return;
+    }
+    else if (!login_mail.value.trim() || !login_password.value.trim()) {
         Swal.fire({
             icon: "error",
             title: "Error! All fields are required",
-            // text: "Something went wrong!",
+        });
+        return;
+    }
+    else if (login_mail.value !== localStorage.getItem("User Email")) {
+        Swal.fire({
+            icon: "error",
+            title: "User email is icorrect",
+            text: "Enter correct email address",
+        });
+        return;
+    }
+    else if (login_mail.value === localStorage.getItem("User Email") && login_password.value !== localStorage.getItem("User Password")) {
+        Swal.fire({
+            icon: "error",
+            title: "User password is incorrect",
+            text: "Enter correct password",
         });
         return;
     }
     else if (login_mail.value === localStorage.getItem("User Email") && login_password.value === localStorage.getItem("User Password")) {
-        alert("code worked")
-        return
-    }
-    else if (login_mail.value !== localStorage.getItem("User Email")){
         Swal.fire({
-            icon: "warning",
-            title: "Email Is Icorrect",
-            text: "Enter Correct Email Address",
+            title: "Login Successfully",
+            icon: "success",
+            draggable: true
         });
+        empty_inputs()
+        return;
     }
 
 }
@@ -101,6 +154,9 @@ login_password.addEventListener('keydown', (key) => {
 
 // Teleporter Start's__________________________
 
+const inputs = document.getElementsByTagName("input")
+let input_flg = true;
+
 const signup = document.getElementById('signup_box');
 const login = document.getElementById('login_box');
 
@@ -109,14 +165,33 @@ function teleporter(params) {
         // console.log(`pass`);
         signup.classList.add('hide');
         login.classList.remove('hide');
-        return
+
+        input_flg = false;
+        // return
     }
     else if (params === 'lgn_to_sgnup') {
         // console.log('pass2');
         login.classList.add('hide');
         signup.classList.remove('hide');
-        return
+
+        input_flg = false;
+        // return
+    }
+
+    if (!input_flg) {
+        empty_inputs()
     }
 }
 
+
 // Teleporter End's__________________________
+
+// All inputs clear function____________________-
+
+function empty_inputs() {
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].value = ""
+    }
+}
+
+// All inputs clear function____________________-
